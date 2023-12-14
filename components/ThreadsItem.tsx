f17e599c96cf532f508b16e1168bf4e8553c3a9c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-15 01:42:24 +0530

    updated

--- components/ThreadsItem.tsx
+++ components/ThreadsItem.tsx
@@ -3,11 +3,13 @@ import * as React from 'react';
 import { Thread } from "../types/threads"
 import { View } from "react-native";
 import { Text } from "./Themed";
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function ThreadsItem(thread: Thread): JSX.Element {
     return (
         <View>
             <Text>{thread.author.name}</Text>
+            
         </View>
     );
 }
@@ -24,7 +26,10 @@ function PostHeading({
     return (
         <View>
             <View>
-                
+                <Text>{name}</Text>
+                {verified && (
+                    <MaterialIcons name="verified" size={14} color="#60a5fa" />
+                )}
             </View>
         </View>
     )
