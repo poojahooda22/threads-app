1586afb581fe3e7344268288c378f0642dc8a584
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-15 01:33:42 +0530

    create thread

---
+++ components/ThreadsItem.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import { Thread } from "../types/threads"
+import { View } from "react-native";
+import { Text } from "./Themed";
+
+export default function ThreadsItem(thread: Thread): JSX.Element {
+    return (
+        <View>
+            <Text>{thread.author.name}</Text>
+        </View>
+    );
+}
