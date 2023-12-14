64f2b9457a1bd1427fa4eab77f271e217efd5ac6
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-14 13:26:14 +0530

    updated

--- types/threads.ts
+++ types/threads.ts
@@ -21,13 +21,13 @@ export interface Reply {
 }
 
 
-// export interface User {
-//     id: string;
-//     name: string;
-//     username: string;
-//     verified: boolean;
-//     photo: string;
-//     bio: string;
-//     link?:string;
-//     followers: 
-// }
+export interface User {
+    id: string;
+    name: string;
+    username: string;
+    verified: boolean;
+    photo: string;
+    bio: string;
+    link?:string;
+    followers: 
+}
