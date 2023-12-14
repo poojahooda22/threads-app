8f0e4de30adb54ab0b9a50d1d7b4c83fd1af62da
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-15 01:33:27 +0530

    updated tabs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,24 @@
 import { StyleSheet, SafeAreaView, ScrollView, Platform, RefreshControl } from 'react-native';
-
+import { useRef } from 'react';
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 import Lottie from 'lottie-react-native';
 import * as React from 'react';
 import LottieView from 'lottie-react-native';
+import { createRandomUser } from '../../utils/generate-dommy-data';
+import { ThreadsContext } from '../../context/thread-context';
+import ThreadsItem from '../../components/ThreadsItem';
+
+
+const user = createRandomUser();
+
+console.log(user);
 
 export default function TabOneScreen() {
-  const animationRef = React.useRef<Lottie>(null)
+  const animationRef = React.useRef<LottieView>(null);
+  const threads = React.useContext(ThreadsContext);
+  // const animationRef = useRef(null);
+
   return (
     <SafeAreaView>
       <ScrollView
@@ -18,18 +29,24 @@ export default function TabOneScreen() {
         refreshControl={
           <RefreshControl
             refreshing={false}
-            onRefresh={() => {}}
+            onRefresh={() => {
+              animationRef.current?.play();
+            }}
             tintColor={"transparent"}
             />
         }
       >
-        <Lottie 
-          // ref={animationRef}
-          source={require('../../lottie-animations/thread2.json')} 
-          autoPlay 
-          loop={true}
-          style={{ width: 90, height: 90, alignSelf: "center" }}
+        <LottieView 
+          ref={animationRef}
+          source={require('../../lottie-animations/thread2.json')}   
+          loop
+          autoPlay
+          style={{ width: 120, height: 120, alignSelf: "center" }}
         />
+
+        {threads.map((thread) => (
+          <ThreadsItem key={thread.id} {...thread}/>
+        ))}
       </ScrollView>
     </SafeAreaView>
   );
