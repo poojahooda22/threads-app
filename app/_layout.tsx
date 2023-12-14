390350d5fb4beeb281248cfac2af941a02dd5566
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-15 01:33:35 +0530

    updated layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
+import { ThreadProvider } from '../context/thread-context';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -47,10 +48,12 @@ function RootLayoutNav() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
+      <ThreadProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        </Stack>
+      </ThreadProvider>
     </ThemeProvider>
   );
 }
