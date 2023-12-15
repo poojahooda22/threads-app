d125d7a4739fd06aad1ee46fd54215b5e534e4c7
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-15 20:21:53 +0530

    updated

--- components/ThreadsItem.tsx
+++ components/ThreadsItem.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { Thread } from "../types/threads"
 import { View } from "react-native";
 import { Text } from "./Themed";
-import { Feather, MaterialIcons } from '@expo/vector-icons';
+import { Feather, FontAwesome, Ionicons, MaterialIcons } from '@expo/vector-icons';
 import { timeAgo } from '../utils/time-ago';
 
 export default function ThreadsItem(thread: Thread): JSX.Element {
@@ -67,6 +67,8 @@ function BottomIcons() {
 
     return (
         <View>
+            <FontAwesome name="heart-o" size={iconSize} color={iconColor} />
+            <Ionicons name="chatbubble-outline" size={iconSize} color={iconColor} />
             
         </View>
     )
