8005a22a25846a903a9b3d33977396cdb43dc3b2
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2023-12-18 00:17:33 +0530

    updated

--- components/ThreadsItem.tsx
+++ components/ThreadsItem.tsx
@@ -195,8 +195,7 @@ const styles = StyleSheet.create({
         height: 40,
         borderRadius: 20,
     },
-    shadowBox: {
-        
+    shadowBox: {    
         backgroundColor: 'white',
         padding: 16,
         borderRadius: 10,
